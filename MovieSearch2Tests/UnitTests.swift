b8c874524fee6d4c5195addad4e6dcb8118a8d3e
Frank Mortensen <fmortens@me.com>
2020-02-24 16:54:42 +0100

    Set up UI tests and specify unit tests of the old

--- MovieSearch2Tests/UnitTests.swift
+++ MovieSearch2Tests/UnitTests.swift
@@ -8,7 +8,7 @@
 
 import XCTest
 
-class MovieSearch2Tests: XCTestCase {
+class UnitTests: XCTestCase {
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
